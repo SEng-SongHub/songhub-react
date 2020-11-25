9ce999dfefd375da672b411ebd17914364aa352a
Ben Sykes <ben@sykesdev.ca>
2020-11-25 15:38:27 -0500

    fuck this shit

--- pages/play.js
+++ pages/play.js
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types'
+import { Grid } from '@material-ui/core'
 import React, { Component } from 'react'
+import Head from 'next/head'
+
+// Components
+import SongPlayer from '../components/SongPlayer/SongPlayer'
+
+// Styles
+import styles from '../styles/Play.module.css'
 
 export default class play extends Component {
 
-    static defaultProps () {
-        return {
-            song: {
-                path: '/files/song.mp3'
-            }
+    constructor(props) {
+        super(props)
+    }
+
+    static defaultProps = {
+        song: {
+            name: 'With a Spirit',
+            artist: '009 Sound System',
+            fileFormat: 'mp3',
+            path: '/files/song.mp3',
         }
     }
 
@@ -16,9 +29,18 @@ export default class play extends Component {
     }
 
     render() {
+        const { song } = this.props
         return (
-            <div>
-                
+            <div className={styles.container}>
+                <Head>
+                    <title>Listening to { song.name }...</title>
+                    <link rel='icon' href='/favicon.ico' />
+                </Head>
+                <Grid container direction="row" justify="center" alignItems="center">
+                    <Grid className={styles.playerContainer} item xs={12}>
+                        <SongPlayer song={song} />
+                    </Grid>
+                </Grid>
             </div>
         )
     }
