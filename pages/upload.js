552de5f7f7210e00865bf43846d373e5884a8c10
Alec Di Vito <divito.alec@gmail.com>
2020-11-28 14:10:41 -0500

    Created Upload page

--- pages/upload.js
+++ pages/upload.js
@@ -1,23 +1,69 @@
-import { Card, Grid, TextField } from '@material-ui/core';
+import { Button, Card, Divider, Grid, Paper, TextField, Typography } from '@material-ui/core';
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
+import Layout from '../components/layout';
+import { makeStyles } from '@material-ui/core/styles';
+import { DropzoneArea } from 'material-ui-dropzone';
 
-export default class Upload extends React.Component {
+const useStyles = makeStyles((theme) => ({
+    container: {
+        height: '90vh',
+    },
+    form: {
+        padding: '4rem',
+        width: 700
+    },
+    paragraph: {
+        marginBottom: '2rem'
+    },
+    input: {
+        width: '100%',
+        paddingBottom: '1rem',
+    },
+    input2: {
+        width: '100%',
+        paddingBottom: '2rem',
+    },
+    btn: {
+        margin: `${theme.spacing(2)}px ${theme.spacing(2)}px 0 0`,
+    }
+}));
+const Upload = () => {
+    const [state, setState] = useState();
+    const [files, setFiles] = useState();
+    const classes = useStyles();
 
-    render() {
-        return (
-            <Grid>
-                <Grid>
-                    <div>upload</div>
-                    <form noValidate autoComplete="off">
-                        <TextField label="Song Name" />
-                        <Card>
-
-                        </Card>
-                    </form>
-                </Grid>
-            </Grid>
-        )
+    const handle = (files) => {
+        setFiles(files);
     }
 
+    return (
+        <Layout>
+            <Grid className={classes.container} container direction="column" justify="center" alignItems="center">
+                <Paper className={classes.form} elevation={3}>
+                    <Typography variant="h2" gutterBottom>Upload</Typography>
+                    <Typography className={classes.paragraph} variant="body1" gutterBottom>
+                        Upload song by inputing the song name, album, and drag
+                        the file into the upload box or click on it to find it
+                        in your file system
+                    </Typography>
+                    <form noValidate autoComplete="off">
+                        <TextField variant="outlined" className={classes.input} name="album" label="Album Name" />
+                        <TextField variant="outlined" className={classes.input2} name="name" label="Song Name" />
+                        <DropzoneArea onChange={handle} />
+                        <Divider />
+                        <Button className={classes.btn} variant="contained" color="primary">
+                            Upload
+                        </Button>
+                        <Button className={classes.btn} variant="contained" color="secondary">
+                            Cancel
+                        </Button>
+                    </form>
+                </Paper>
+            </Grid>
+        </Layout>
+    )
+
 }
+
+export default Upload;
