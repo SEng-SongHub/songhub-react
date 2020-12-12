0ad536d9ccd801c22c0b338f8c564b23e9313d7e
Ben Sykes <ben@sykesdev.ca>
2020-12-12 10:40:57 -0500

    authentication implemented from backend service

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
+import axios from 'axios'
 import styles from '../styles/Home.module.css'
+import Router from 'next/router'
+import Collapse from '@material-ui/core/Collapse'
+import { v4 as uuid } from 'uuid'
 
-export default class Home extends React.Component {
-  render () {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Create Next App</title>
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
+export default function Home () {
+	const [ inputState, setInputState ] = useState({
+		email    : '',
+		password : ''
+	})
+	const [ errors, setErrors ] = useState([])
 
-        <main className={styles.signupMain}>
-          <h1 className={styles.title}>
-            <a>Welcome to SongHub!</a>
-          </h1>
+	const handleInputChange = (evt) => {
+		setInputState({
+			...inputState,
+			[evt.target.name]: evt.target.value
+		})
+	}
 
-          <p className={styles.description}>Get started by logging in or creating a new account!</p>
+	const login = async (evt) => {
+		evt.preventDefault()
 
-          <div className={styles.vertical}>
-            <h1>Log In</h1>
+		try {
+			const res = await axios({
+				method       : 'POST',
+				url          : 'http://localhost:5000/authentication/login',
+				data         : inputState,
+				responseType : 'json'
+			})
 
-            <input type='text' id='email' name='email' className={styles.textbox} placeHolder='Email' />
+			// Successful Login
+			console.log(res.data)
+			if (res.status === 200 || res.status === 201) {
+				Router.push('/upload')
+			}
+		} catch (err) {
+			console.log(err)
+			setErrors([ 'Invalid Credentials' ])
+		}
+	}
 
-            <input
-              type='text'
-              id='password'
-              name='password'
-              className={styles.textbox}
-              placeHolder='Password'
-            />
-          </div>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>SongHub</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-          <a href='upload' className={styles.signup}>
-						Log In
-          </a>
+			<main className={styles.signupMain}>
+				<h1 className={styles.title}>
+					<a>Welcome to SongHub!</a>
+				</h1>
 
-          <h2>-- OR --</h2>
+				<p className={styles.description}>Get started by logging in or creating a new account!</p>
 
-          <a href='signup' className={styles.signup}>
-						Sign up!
-          </a>
-        </main>
+				<div className={styles.vertical}>
+					<h1>Log In</h1>
 
-        <footer className={styles.footer}>
-          <a target='_blank' rel='noopener noreferrer'>
-						Powered by Sick Beats
-          </a>
-        </footer>
-      </div>
-    )
-  }
+					{errors ? (
+						<Collapse className={styles.errorContainer} in={errors.length > 0}>
+							<div>
+								<ul>{errors.map((error) => <li key={uuid()}>{error}</li>)}</ul>
+							</div>
+						</Collapse>
+					) : null}
+
+					<input
+						type='text'
+						id='email'
+						name='email'
+						onChange={handleInputChange}
+						className={styles.textbox}
+						placeholder='Email'
+					/>
+
+					<input
+						type='text'
+						id='password'
+						name='password'
+						onChange={handleInputChange}
+						className={styles.textbox}
+						placeholder='Password'
+					/>
+				</div>
+
+				<a href='#' onClick={login} className={styles.signup}>
+					Log In
+				</a>
+
+				<h2>-- OR --</h2>
+
+				<a href='signup' className={styles.signup}>
+					Sign up!
+				</a>
+			</main>
+
+			<footer className={styles.footer}>
+				<a target='_blank' rel='noopener noreferrer'>
+					Powered by Sick Beats
+				</a>
+			</footer>
+		</div>
+	)
 }
