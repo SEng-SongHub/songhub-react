e03361a454e83e22a7698075bafc7a0eff2959a2
Ben Sykes <ben@sykesdev.ca>
2020-12-11 23:40:37 -0500

    added backend implementation to upload component

--- pages/upload.js
+++ pages/upload.js
@@ -1,75 +1,133 @@
 import { Button, Divider, Grid, Paper, TextField, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
 import { makeStyles } from '@material-ui/core/styles'
 import { DropzoneArea } from 'material-ui-dropzone'
+import axios from 'axios'
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    height: '90vh'
-  },
-  form: {
-    padding: '4rem',
-    width: 700
-  },
-  paragraph: {
-    marginBottom: '2rem'
-  },
-  input: {
-    width: '100%',
-    paddingBottom: '1rem'
-  },
-  input2: {
-    width: '100%',
-    paddingBottom: '2rem'
-  },
-  btn: {
-    margin: `${theme.spacing(2)}px ${theme.spacing(2)}px 0 0`
-  }
+	container : {
+		height : '90vh'
+	},
+	form      : {
+		padding : '4rem',
+		width   : 700
+	},
+	paragraph : {
+		marginBottom : '2rem'
+	},
+	input     : {
+		width         : '100%',
+		paddingBottom : '1rem'
+	},
+	input2    : {
+		width         : '100%',
+		paddingBottom : '2rem'
+	},
+	btn       : {
+		margin : `${theme.spacing(2)}px ${theme.spacing(2)}px 0 0`
+	}
 }))
 const Upload = () => {
-  const [_, setFiles] = useState()
-  const classes = useStyles()
+	const [ files, setFiles ] = useState()
+	const [ inputState, setInputState ] = useState({
+		name   : '',
+		artist : ''
+	})
+	const classes = useStyles()
 
-  const handle = (files) => {
-    console.log(_)
-    setFiles(files)
-  }
+	const handle = (selectedFiles) => {
+		console.log(selectedFiles)
+		setFiles(selectedFiles)
+	}
 
-  return (
-    <Layout>
-      <Head>
-        <title>Upload Song</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Grid className={classes.container} container direction='column' justify='center' alignItems='center'>
-        <Paper className={classes.form} elevation={3}>
-          <Typography variant='h2' gutterBottom>
+	const handleFieldChange = (evt) => {
+		setInputState({
+			...inputState,
+			[evt.target.name]: evt.target.value
+		})
+	}
+
+	const handleUpload = (evt) => {
+		evt.preventDefault()
+		uploadFiles(inputState.name, inputState.artist)
+	}
+
+	const uploadFiles = async (name, artist) => {
+		console.log(files)
+		if (files.length === 1) {
+			let formData = new FormData()
+			formData.append('file', files[0])
+			formData.append('name', name)
+			formData.append('artist', artist)
+
+			// Make request to backend
+			try {
+				const res = await axios({
+					method       : 'POST',
+					data         : formData,
+					responseType : 'json',
+					url          : 'http://localhost:5000/management/song'
+				})
+
+				if (res.status === 200) {
+					alert('Files uploaded!')
+				} else {
+					console.log(res.data)
+				}
+			} catch (err) {
+				console.log(`Failed to upload files. Reason: ${err}`)
+			}
+		}
+	}
+
+	return (
+		<Layout>
+			<Head>
+				<title>Upload Song</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Grid className={classes.container} container direction='column' justify='center' alignItems='center'>
+				<Paper className={classes.form} elevation={3}>
+					<Typography variant='h2' gutterBottom>
 						Upload
-          </Typography>
-          <Typography className={classes.paragraph} variant='body1' gutterBottom>
+					</Typography>
+					<Typography className={classes.paragraph} variant='body1' gutterBottom>
 						Upload song by inputing the song name, album, and drag the file into the upload box or click on
 						it to find it in your file system
-          </Typography>
-          <form noValidate autoComplete='off'>
-            <TextField variant='outlined' className={classes.input} name='album' label='Album Name' />
-            <TextField variant='outlined' className={classes.input2} name='name' label='Song Name' />
-            <DropzoneArea onChange={handle} />
-            <Divider />
-            <a href='play'>
-              <Button className={classes.btn} variant='contained' color='primary'>
+					</Typography>
+					<form noValidate autoComplete='off'>
+						<TextField variant='outlined' className={classes.input} name='album' label='Album Name' />
+						<TextField
+							variant='outlined'
+							className={classes.input}
+							onChange={handleFieldChange}
+							name='artist'
+							label='Artist Name'
+						/>
+						<TextField
+							variant='outlined'
+							className={classes.input2}
+							onChange={handleFieldChange}
+							name='name'
+							label='Song Name'
+						/>
+						<DropzoneArea onChange={handle} />
+						<Divider />
+						<a href='play'>
+							<Button className={classes.btn} variant='contained' onClick={handleUpload} color='primary'>
 								Upload
-              </Button>
-            </a>
-            <Button className={classes.btn} variant='contained' color='secondary'>
+							</Button>
+						</a>
+						<Button className={classes.btn} variant='contained' color='secondary'>
 							Cancel
-            </Button>
-          </form>
-        </Paper>
-      </Grid>
-    </Layout>
-  )
+						</Button>
+					</form>
+				</Paper>
+			</Grid>
+		</Layout>
+	)
 }
 
 export default Upload
