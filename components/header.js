719f7ae71cc9b52d52a1b05c239ee05fa202511b
Alec Di Vito <divito.alec@gmail.com>
2020-12-16 11:27:11 -0500

    Updated header to use style file

--- components/header.js
+++ components/header.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Link from 'next/link'
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-}))
+import styles from '../styles/Header.module.css'
 
 const Header = () => {
-  const classes = useStyles()
-
   return (
     <AppBar position="static">
-      <Toolbar className={classes.root}>
+      <Toolbar className={styles.root}>
         <Typography variant="h6">
           SongHub
       </Typography>
